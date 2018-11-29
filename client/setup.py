f58e248b0b3f08e836accbe733c4bf57fac379c0
Justin Azoff <justin@corelight.com>
2018-11-29 11:47:56 -0500

    rename client to tryzeek

--- client/setup.py
+++ client/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='trybro',
+setup(name='tryzeek',
     version='0.2.0',
     zip_safe=True,
-    py_modules = ["trybro"],
+    py_modules = ["tryzeek"],
     install_requires='requests',
     entry_points = {
         'console_scripts': [
-            'trybro = bro:main',
+            'tryzeek = tryzeek:main',
         ]
     }
 )
