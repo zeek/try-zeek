3410a16214e818be285babafa354e121cf3fd8a6
Benjamin Bannier <benjamin.bannier@corelight.com>
2024-07-29 12:19:23 +0200

    Add missing import
    Without this development mode (`npm run start`) is broken on my machine
    and the text editor is not displayed.

--- manager/web-ui/src/ZeekMode.js
+++ manager/web-ui/src/ZeekMode.js
@@ -1,7 +1,8 @@
+import window from 'brace';
 import 'brace/mode/java';
 
-export class ZeekHighlightRules extends window.ace.acequire("ace/mode/text_highlight_rules").TextHighlightRules {
+export class ZeekHighlightRules extends window.acequire("ace/mode/text_highlight_rules").TextHighlightRules {
 }
 
-export default class ZeekMode extends window.ace.acequire('ace/mode/java').Mode {
+export default class ZeekMode extends window.acequire('ace/mode/java').Mode {
 }
