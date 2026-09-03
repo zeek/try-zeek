9c32095ed08b20b91305bb009c680a70f1564294
Benjamin Bannier <benjamin.bannier@corelight.com>
2026-09-03 08:46:06 +0200

    Fix test to render App with a Redux store
    The test was passing reducer state as flat props to the unconnected `App`,
    but `componentDidMount` calls `this.props.dispatch` which only exists on
    the connected component. Render the connected `App` inside a `Provider`
    instead, and mock `isomorphic-fetch` so API calls resolve without a server.

--- manager/web-ui/src/App.test.js
+++ manager/web-ui/src/App.test.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './App';
-
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import App from './App';
 import tryBroApp from './reducers';
 
+// App dispatches API fetches on mount; stub fetch so they resolve without a server.
+jest.mock('isomorphic-fetch', () => () =>
+  Promise.resolve({ ok: true, json: () => Promise.resolve({}) }),
+);
+
 it('renders without crashing', () => {
+  const store = createStore(tryBroApp, applyMiddleware(thunk));
   const div = document.createElement('div');
-  var props = tryBroApp(undefined, {type:null})
-  ReactDOM.render(<App {...props}/>, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div,
+  );
 });
